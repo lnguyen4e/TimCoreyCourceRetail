17c71ddb6ff63bcf42226d026a861810d91f45b9
Lam B Nguyen <l0nguy10@louisville.edu>
2020-09-17 16:22:59 -0400

    Register DB table
    The table for the basic register system

---
+++ TimCoRetailManager/TRMData/dbo/Table/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	
+	[Id] nvarchar(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NCHAR(55) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate() 
+)
