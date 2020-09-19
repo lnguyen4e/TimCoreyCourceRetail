86adb67845b8ec9a58faff0dfaf0f206a287c64d
Lam B Nguyen <l0nguy10@louisville.edu>
2020-09-18 22:13:19 -0400

    Continue with API
    add  a post route

---
+++ TimCoRetailManager/TRM DataManager/Controllers/SaleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRM_DataManager.Controllers
+{
+    public class SaleController : ApiController
+    {[Authorize]
+        public void Post(SaleModel sale)
+        {
+            SaleData data = new SaleData();
+            string userid = RequestContext.Principal.Identity.GetUserId();
+            data.SaveSale(sale, userid);
+        }
+        //public List<ProductModel> Get()
+        //{
+        //    ProductData data = new ProductData();
+        //    return data.GetProducts();
+
+        //}
+    }
+}
